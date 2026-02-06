2fa95ff1e81a5a402d6982ab78df343c99a98b5a
Tasuku Yamashita <task.teckac@gmail.com>
2026-02-06 13:51:31 +0900

    state: split state db per GIONX_ROOT

--- internal/cli/init.go
+++ internal/cli/init.go
@@ -41,7 +41,7 @@ func (c *CLI) runInit(args []string) int {
 		return exitError
 	}
 
-	dbPath, err := paths.DefaultStateDBPath()
+	dbPath, err := paths.StateDBPathForRoot(root)
 	if err != nil {
 		fmt.Fprintf(c.Err, "resolve state db path: %v\n", err)
 		return exitError
